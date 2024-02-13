90f4a9a6a641f603108e82fc32823e885b53649e
sandeep066 <12527032+sandeep066@users.noreply.github.com>
2024-02-13 07:37:01 +0530

    Update throttle working code in polyfills.js

--- polyfills.js
+++ polyfills.js
@@ -24,3 +24,36 @@ debouncedSearch("Debounced search 3");
 Searching...: Debounced search 3
 
 +++++++++++++++++++++++++++++
+
+//throttle
+function throttle(func,delay){
+  let lastTime=0;
+    return function(...args){
+      let currentTime=Date.now()
+      if(currentTime-lastTime>=delay){
+          func(...args)
+        lastTime=currentTime
+      }
+    }
+}
+function fetchApidata(data){
+  console.log("Fetched api data",data)
+}
+
+throttledFetch=throttle(fetchApidata,1000)
+
+throttledFetch("Fetch api 1 called !")
+throttledFetch("Fetch api 2 called !")
+throttledFetch("Fetch api 3 called !")
+// After 1 second, if no more function calls, next call will be executed
+setTimeout(() => {
+      throttledFetch("Fetch api 4 called !"); 
+}, 1000);
+
+//output
+Fetched api data Fetch api 1 called !
+Fetched api data Fetch api 4 called !
+
+//if setTimeout set to < 1000 like 700 in last call,
+Fetched api data Fetch api 1 called ! 
+
